d8f103bdf3c2dbb0c13f489bbccf53608bf81524
KehnAdrian31 <kehn000panganibangmail.com>
2025-08-04 16:17:20 +0200

    [upd] import express, definizione porta, add images

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+// importo express
+const express = require ('express');
+
+// chiamo la funzione per creare l' oggetto express e salvarlo in una variabile
+const app = express();
+
+//definisco la porta in cui il server deve rimanere in ascolto
+const port = 3000;
+
